9187c7071e6970871a52758a6fcd944070f594dc
Beregszászi István <beistvan@gmail.com>
2023-08-04 12:51:51 +0200

    Easy Task

---
+++ easy-task.py
@@ -0,0 +1,9 @@
+import math
+a, b = int(input()), int(input())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(math.sqrt(math.pow(a, 10) + math.pow(b, 10)))
