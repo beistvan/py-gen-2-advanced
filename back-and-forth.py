2d1930d9fa5e64f14550a77ceb64fd360f01752e
Beregszászi István <beistvan@gmail.com>
2023-08-07 12:19:27 +0200

    Back And Forth

---
+++ back-and-forth.py
@@ -0,0 +1,6 @@
+numbers = [int(n) for n in input().split()]
+
+for i in range(0, len(numbers) - 1, 2):
+    numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+
+print(' '.join(str(i) for i in numbers))
