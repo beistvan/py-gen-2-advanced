11a6ebdf35d03923a19d5b346a47afb49025067f
Beregszászi István <beistvan@gmail.com>
2023-08-12 12:35:01 +0200

    Rock Paper Scissors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,6 +1,5 @@
 a, b = input(), input()
-game = {'stone-stone': 'draw', 'stone-scissors': 'John', 'stone-paper': 'Jack',
-        'scissors-scissors': 'draw','scissors-paper': 'John', 'scissors-stone': 'Jack',
-        'paper-paper': 'draw','paper-stone': 'John','paper-scissors': 'Jack'}
+game = {'rock-rock': 'draw', 'rock-scissors': 'John', 'rock-paper': 'Jack',
+        'scissors-scissors': 'draw','scissors-paper': 'John', 'scissors-rock': 'Jack',
+        'paper-paper': 'draw','paper-rock': 'John','paper-scissors': 'Jack'}
 print(game[a + "-" + b])
-
